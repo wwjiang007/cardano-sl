11b3ea01a88215bca9d83c84659ca0cae8dca42c
Denis Shevchenko <denis.shevchenko@iohk.io>
2018-04-16 17:21:24 +0400

    [CHW-82] External wallet WIP

--- wallet-new/server/Cardano/Wallet/API/V1/LegacyHandlers/Wallets.hs
+++ wallet-new/server/Cardano/Wallet/API/V1/LegacyHandlers/Wallets.hs
@@ -4,21 +4,29 @@ import           Universum
 
 import qualified Pos.Wallet.Web.ClientTypes.Types as V0
 import qualified Pos.Wallet.Web.Methods as V0
-import qualified Pos.Wallet.Web.State as V0 (WalletSnapshot, askWalletSnapshot)
-import qualified Pos.Wallet.Web.State.Storage as V0
+import qualified Pos.Wallet.Web.Account as V0
+import qualified Pos.Wallet.Web.State as V0 (WalletSnapshot, askWalletSnapshot, askWalletDB, removeHistoryCache, setWalletSyncTip)
+import qualified Pos.Wallet.Web.State.Storage as V0 hiding (removeHistoryCache, setWalletSyncTip)
 
+import           Pos.Core (makePubKeyAddressBoot)
+import           Pos.Util.Servant (encodeCType)
+import           Pos.Crypto (PublicKey (..), firstHardened)
 import           Cardano.Wallet.API.Request
 import           Cardano.Wallet.API.Response
 import           Cardano.Wallet.API.V1.Errors
 import           Cardano.Wallet.API.V1.Migration
 import           Cardano.Wallet.API.V1.Types as V1
 import qualified Cardano.Wallet.API.V1.Wallets as Wallets
+import           Cardano.Crypto.Wallet (xpub)
 import qualified Data.IxSet.Typed as IxSet
 import           Pos.Update.Configuration ()
+import           Pos.StateLock (Priority (..), withStateLockNoMetrics)
 
 import           Pos.Wallet.Web.Methods.Logic (MonadWalletLogic, MonadWalletLogicRead)
 import           Servant
 import           Test.QuickCheck (arbitrary, generate)
+import           Data.ByteString.Base58 (bitcoinAlphabet, decodeBase58)
+import           Data.Maybe (isNothing, fromJust)
 
 -- | All the @Servant@ handlers for wallet-specific operations.
 handlers :: ( HasConfigurations
@@ -131,8 +139,59 @@ newExternalWallet
     :: (MonadThrow m, MonadWalletLogic ctx m)
     => NewExternalWallet
     -> m (WalletResponse Wallet)
-newExternalWallet _ =
-    single <$> (liftIO $ generate arbitrary)
+newExternalWallet NewExternalWallet{..} = do
+    let newWalletHandler CreateWallet  = createNewExternalWallet
+        newWalletHandler RestoreWallet = error "TODO"
+    walletMeta <- V0.CWalletMeta <$> pure newewalName
+                                 <*> migrate newewalAssuranceLevel
+                                 <*> pure 0
+    single <$> do
+        v0wallet <- newWalletHandler newewalOperation walletMeta newewalExtPubKey
+        ss <- V0.askWalletSnapshot
+        addWalletInfo ss v0wallet
+
+-- | Creates new external wallet.
+--
+-- There's no spending password, because it's assumed that
+-- extenal wallet has its own security mechanism.
+-- For example, in Ledger Nano S device it's 4 digits PIN-code.
+--
+-- There's no backup phrase as well, because it's assumed that
+-- external wallet already received backup phrase, created
+-- a secret key based on it and stored this key.
+createNewExternalWallet
+    :: (MonadThrow m, MonadWalletLogic ctx m)
+    => V0.CWalletMeta
+    -> Text
+    -> m V0.CWallet
+createNewExternalWallet walletMeta encodedExtPubKey = do
+    let rawExtPubKey = decodeBase58 bitcoinAlphabet . encodeUtf8 $ encodedExtPubKey
+    when (isNothing rawExtPubKey) $ throwM (UnkownError "Ext public key is not in proper Base58-form.")
+
+    let extPubKey = xpub $ fromJust rawExtPubKey
+    when (isLeft extPubKey) $ throwM (UnkownError "Invalid ext public key.")
+
+    let (xPub:_) = rights [extPubKey] -- We already know that it's 'Right'.
+        walletId = encodeCType . makePubKeyAddressBoot $ PublicKey xPub
+        isReady  = True -- A brand new wallet doesn't need syncing with the blockchain.
+
+    V0.CWallet{..} <- V0.createWalletSafe walletId walletMeta isReady
+    -- Can't return this result, since balances can change.
+
+    let accountMeta    = V0.CAccountMeta { caName = "Initial account" }
+        accountInit    = V0.CAccountInit { caInitWId = cwId, caInitMeta = accountMeta }
+        includeUnready = True
+        passphrase     = mempty
+    void $ V0.newAccountIncludeUnready includeUnready
+                                       (V0.DeterminedSeed firstHardened)
+                                       passphrase
+                                       accountInit
+    db <- V0.askWalletDB
+    V0.removeHistoryCache db walletId
+    -- BListener checks current syncTip before applying update,
+    -- thus setting it up to date manually here
+    withStateLockNoMetrics HighPriority $ \tip -> V0.setWalletSyncTip db walletId tip
+    V0.getWallet walletId
 
 -- | Creates a new BIP44 derivation path for an external wallet.
 newAddressPath
